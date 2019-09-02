c1485b645582c773688504ef0bd72ea761a80471
piyush-jaiswal <piyush.jaiswal@st.niituniversity.in>
2019-09-02 11:33:17 +0530

    Clean up css and js

--- js/jquery.bookblock.custom.js
+++ js/jquery.bookblock.custom.js
@@ -1,14 +1,3 @@
-/**
- * jquery.bookblock.js v2.0.1
- * http://www.codrops.com
- *
- * Licensed under the MIT license.
- * http://www.opensource.org/licenses/mit-license.php
- * 
- * Copyright 2013, Codrops
- * http://www.codrops.com
- */
-
 (function ($, window, undefined) {
 
 	'use strict';
